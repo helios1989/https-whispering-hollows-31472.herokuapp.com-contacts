380ad005ebc09a6813ec30c243e34cd554b9ebd3
Vergel <vergel@mokucollective.com>
2017-06-13 14:11:16 +0800

    added server js chagnes

--- server.js
+++ server.js
@@ -56,6 +56,16 @@ app.get("/api/contacts", function(req, res) {
   });
 });
 
+app.get("/api/friends", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
 app.post("/api/contacts", function(req, res) {
   var newContact = req.body;
   newContact.createDate = new Date();
@@ -73,22 +83,7 @@ app.post("/api/contacts", function(req, res) {
   });
 });
 
-app.post("/api/friends", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
 
-  if (!req.body.name) {
-    handleError(res, "Invalid users input", "Must provide a name.", 400);
-  }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
 
 
 /*  "/api/contacts/:id"
